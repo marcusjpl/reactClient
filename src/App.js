62cdac58f65712b84c9f4f3dd0c39d006adba1a6
marcusjpl <marcusjpl@gmail.com>
2018-07-20 21:03:13 -0300

    Adicionando bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Navbar, Jumbotron, Button, Badge, ButtonToolbar } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +14,25 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+        {/* Standard button  https://react-bootstrap.github.io/components/buttons/*/}
+        <ButtonToolbar>
+
+        <Button>Default</Button>
+        {/* Provides extra visual weight and identifies the primary action in a set of buttons */}
+        <Button bsStyle="primary">Primary</Button>
+        {/* Indicates a successful or positive action */}
+        <Button bsStyle="success">Success</Button>
+        {/* Contextual button for informational alert messages */}
+        <Button bsStyle="info">Info</Button>
+        {/* Indicates caution should be taken with this action */}
+        <Button bsStyle="warning">Warning</Button>
+        {/* Indicates a dangerous or potentially negative action */}
+        <Button bsStyle="danger">Danger</Button>
+        {/* Deemphasize a button by making it look like a link while maintaining button behavior */}
+        <Button bsStyle="link">Link</Button>
+      </ButtonToolbar>;
+
       </div>
     );
   }
