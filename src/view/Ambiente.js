fd4f73364d49c7e8862092fcbdf635252e4bdc1d
marcusjpl <marcusjpl@gmail.com>
2018-07-29 23:42:36 -0300

    formulario

--- src/view/Ambiente.js
+++ src/view/Ambiente.js
@@ -1,13 +1,42 @@
 import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import { Button, FormGroup, Form, ControlLabel, FormControl, Col, Checkbox } from 'react-bootstrap';
 import $ from 'jquery';
 
 class Ambiente extends Component {
+
+
   render() {
     return (
       <div className="App">
 
           <h1 className="page-header">Ambiente</h1>
+
+          <Form horizontal>
+              <FormGroup controlId="formHorizontalEmail">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Nome
+                </Col>
+                  <Col sm={6}>
+                  <FormControl type="text" />
+                </Col>
+              </FormGroup>
+
+              <FormGroup controlId="formHorizontalPassword">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Descrição
+                </Col>
+                <Col sm={6}>
+                  <FormControl type="text" />
+                </Col>
+              </FormGroup>
+
+              <FormGroup>
+                <Col smOffset={2} sm={6}>
+                  <Button bsStyle="primary">Salvar</Button>
+                  <Button>Limpar</Button>
+                </Col>
+              </FormGroup>
+          </Form>
 
 
       </div>
